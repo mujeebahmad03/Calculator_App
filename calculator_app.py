6717dcec611dc73246fbb8fc3b254f9d479cf8a7
Abdulmujeeb Ahmad <abdulmujeebahmad03@gmail.com>
2023-08-11 11:23:22 +0100

    Fixed unregistered keypress and updated a operation function

--- calculator_app.py
+++ calculator_app.py
@@ -1,15 +1,19 @@
 import tkinter as tk
 from tkinter import ttk
 import math
+import numpy as np
 from functools import partial
 from decimal import Decimal
+
 
 # Logging Function/Method Decorator
 def log_function_call(func):
     def wrapper(*args, **kwargs):
         print(f'calling function: {func.__name__}')
         return func(*args, **kwargs)
+
     return wrapper
+
 
 class Calculator:
     """
@@ -23,11 +27,11 @@ class Calculator:
         self.text = tk.Text(self.root, width=50, height=5, font=('Arial', 16))
         self.text.grid(row=0, column=0, columnspan=7)
         self.memory = 0
-        
+
         self.style = ttk.Style()
-        self.style.configure('Numeric.TButton', background='green')
-        self.style.configure('Operator.TButton', background='yellow')
-        self.style.configure('Memory.TButton', background='SystemTransparent')
+        self.style.configure('Numeric.TButton', background='#D3D3D3')
+        self.style.configure('Operator.TButton', background='#FFA500')
+        self.style.configure('Memory.TButton', background='#4ADEDE')
         self.create_buttons()
 
     @log_function_call
@@ -36,7 +40,7 @@ class Calculator:
         Create calculator buttons with appropriate styling.
         """
         button_texts = [
-            'MC','M+','M-','MR','C', '%', '/',
+            'MC', 'M+', 'M-', 'MR', 'C', '%', '/',
             '√', '^2', '^3', '7', '8', '9', '*',
             'sin', 'cos', 'tan', '4', '5', '6', '+',
             '1/x', '3√', 'log', '1', '2', '3', '-',
@@ -50,11 +54,11 @@ class Calculator:
             button = ttk.Button(self.root, text=text, width=3, style=style_name,
                                 command=partial(self.handle_button_click, text))
             button.grid(row=row, column=col)
-            col += 1 
+            col += 1
             if col > 6:
                 col = 0
                 row += 1
-    
+
     @log_function_call
     def get_button_style(self, button_text):
         """
@@ -64,7 +68,7 @@ class Calculator:
             return 'Operator.TButton'
         elif button_text in ['M+', 'M-', 'MC', 'MR']:
             return 'Memory.TButton'
-        else: 
+        else:
             return 'Numeric.TButton'
 
     @log_function_call
@@ -73,7 +77,7 @@ class Calculator:
         Perform a mathematical operation based on the given expression.
         """
         try:
-            result = eval(expression, {'__builtins__': None}, {'Decimal': Decimal})   
+            result = eval(expression, {'__builtins__': None}, {'Decimal': Decimal})
             if result == float("inf"):
                 raise ZeroDivisionError
             return self.update_text(str(result))
@@ -83,8 +87,8 @@ class Calculator:
             self.update_text('Invalid Input')
         except Exception as error:
             return self.update_text(f'Error: {error}')
-    
-    @log_function_call        
+
+    @log_function_call
     def update_text(self, value):
         """
         Update the text area with the given value.
@@ -97,8 +101,8 @@ class Calculator:
         """
         Display an error message in the text area.
         """
-        self.update_text(f'Error, {message}' )
-    
+        self.update_text(f'Error, {message}')
+
     @log_function_call
     def perform_unary_operation(self, operation, input_type):
         """
@@ -107,7 +111,7 @@ class Calculator:
         try:
             if input_type == 'unary':
                 value = float(self.text.get(1.0, tk.END))
-                if (operation == math.sqrt or operation == (lambda x: x ** (1/3))) and value < 0:
+                if operation == math.sqrt and value < 0:
                     self.error_handler('Not a number')
                     return
             if input_type == 'integer':
@@ -152,21 +156,21 @@ class Calculator:
             except ValueError:
                 num = None
         return num
-    
+
     @log_function_call
     def clear_text(self):
         """
         Clear the text area.
         """
         self.text.delete(1.0, tk.END)
-    
+
     @log_function_call
     def handle_button_click(self, text):
         """
         Handle button clicks.
         """
         current_entry = self.text.get(1.0, 'end').strip()
-        
+
         # Input Validation
         if text.isdigit() or text in '+-/.*()':
             self.text.insert(tk.END, text)
@@ -179,7 +183,7 @@ class Calculator:
             elif text == '√':
                 self.perform_unary_operation(math.sqrt, 'unary')
             elif text == '3√':
-                self.perform_unary_operation(lambda x: x ** (1/3), 'unary')
+                self.perform_unary_operation(lambda x: np.cbrt(x), 'unary')
             elif text == '^2':
                 self.perform_unary_operation(lambda x: x ** 2, 'unary')
             elif text == '^3':
@@ -189,7 +193,7 @@ class Calculator:
             elif text == '+/-':
                 self.perform_unary_operation(lambda x: x * (-1), 'integer')
             elif text == '1/x':
-                self.perform_unary_operation(lambda x: 1/x, 'unary')
+                self.perform_unary_operation(lambda x: 1 / x, 'unary')
             elif text == 'log':
                 self.perform_unary_operation(math.log10, 'unary')
             elif text == 'sin':
@@ -217,7 +221,7 @@ class Calculator:
 
         if symbol and symbol in '1234567890':
             self.handle_button_click(key)
-        elif key and key in '+-/*()%':
+        elif key and key in '+-/*()%.':
             self.handle_button_click(key)
         elif key == '=' or symbol == 'Return':
             self.perform_operation(current_entry)
@@ -226,6 +230,7 @@ class Calculator:
         elif symbol == 'BackSpace':
             updated_entry = current_entry[:-1]
             self.update_text(updated_entry)
+
 
 if __name__ == '__main__':
     root = tk.Tk()
